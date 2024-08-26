e5e05ecf1023edbf44bde27ea904b6ff9c3ff9a4
Alex Frutkin <afrutkin@uci.edu>
2024-08-25 17:11:16 -0700

    Initial commit, base template

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import ProTip from './ProTip';
+import LandingPage from './LandingPage';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <LandingPage />
+  );
+}
