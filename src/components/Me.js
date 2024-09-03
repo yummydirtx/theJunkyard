875dbde9bce61ea7ca4061b04c36f44208afc69d
Alex Frutkin <frutkinalexander@gmail.com>
2024-09-02 19:17:23 -0700

    removed hero image

--- src/components/Me.js
+++ src/components/Me.js
@@ -7,7 +7,6 @@ import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import lands from '../assets/junkyard.webp';
 import ProfilePic from '../assets/profilepic.jpeg';
 
 export default function Me() {
@@ -77,27 +76,6 @@ export default function Me() {
           src = {ProfilePic}
         />
         </Stack>
-        <Box
-          component="img"
-          id="image"
-          sx={(theme) => ({
-            mt: { xs: 8, sm: 10 },
-            alignSelf: 'center',
-            height: { xs: 200, sm: 700 },
-            width: '100%',
-            borderRadius: '10px',
-            outline: '1px solid',
-            outlineColor:
-              theme.palette.mode === 'light'
-                ? alpha('#BFCCD9', 0.5)
-                : alpha('#9CCCFC', 0.1),
-            boxShadow:
-              theme.palette.mode === 'light'
-                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
-                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
-          })}
-          src = {lands}
-        />
       </Container>
     </Box>
   );
