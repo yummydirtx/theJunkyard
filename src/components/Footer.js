315391461e6fb350060c2ac0e1951ec58d71c7c7
Alex Frutkin <frutkinalexander@gmail.com>
2025-03-18 17:16:00 -0700

    Remove unused imports and add copyright notice in relevant components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,17 +19,14 @@
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Logo from '../assets/websitelogo.png';
 import FacebookIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/X';
 
 const logoStyle = {
   width: '100px',
