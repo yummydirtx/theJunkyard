7f47910b8ab1970d3cde2810fdfa720eb194c365
Alex Frutkin <frutkinalexander@gmail.com>
2025-03-22 14:08:28 -0700

    Refactor useTitle to import useEffect directly from React for cleaner code

--- src/components/useTitle.js
+++ src/components/useTitle.js
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import { useEffect } from 'react';
 
 export function useTitle(title) {
-  React.useEffect(() => {
+  useEffect(() => {
     const prevTitle = document.title
     document.title = title
     return () => {
