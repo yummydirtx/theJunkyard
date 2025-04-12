c35750ae35cd01248d1aafdfbca4d3c7517d87d8
Alex Frutkin <frutkinalexander@gmail.com>
2025-04-11 22:27:58 -0700

    Remove unused import

--- src/components/AccountSettingsModal.jsx
+++ src/components/AccountSettingsModal.jsx
@@ -17,7 +17,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THEJUNKYARD OR THE USE OR OTHER DEALINGS IN THEJUNKYARD.
 
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
   Dialog,
   DialogTitle,
