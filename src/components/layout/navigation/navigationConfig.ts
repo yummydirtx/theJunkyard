ee06e9a289d8c6eb4161dfb59677892327179f62
Alex Frutkin <frutkinalexander@gmail.com>
2025-11-08 17:28:13 -0800

    Ensured that navigationconfig is correct

--- src/components/layout/navigation/navigationConfig.ts
+++ src/components/layout/navigation/navigationConfig.ts
@@ -10,11 +10,12 @@ export interface NavigationItem {
 export const mainNavigationItems: NavigationItem[] = [
   { label: 'Home', href: '/' },
   { label: 'Manual Budget', href: '/manualbudget' },
-  { label: 'Expense Report', href: '/expensereport' },
+  { label: 'Keystone', href: 'https://gokeystone.org', external: true, icon: LaunchIcon },
   { label: 'AnteaterFind', href: 'https://anteaterfind.com', external: true, icon: LaunchIcon },
 ];
 
 export const moreNavigationItems: NavigationItem[] = [
+  { label: 'Expense Report', href: '/expensereport' },
   { label: 'calcBasic', href: '/calcbasic-web' },
   { label: 'YTThumb', href: '/ytthumb' },
 ];
